9c4ab186d5611091ebd7a89ceb3a699b887c4770
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-10-27 13:41:42 -0600

    no change

--- routes/api/tasks/index.ts
+++ routes/api/tasks/index.ts
@@ -7,12 +7,12 @@ const tasks = db.collection<TaskSchema>('tasks');
 export const handler: Handlers<Task | null> = {
   async GET(_req, _ctx) {
     const allTasks = await tasks.find({}).toArray();
-    const body = { tasks: allTasks }
+    const body = { tasks: allTasks };
     return new Response(JSON.stringify(body));
   },
 
   async POST(req, _ctx) {
-    const task = (await req.json()) as Task
+    const task = await req.json() as Task;
     const body = {
       message: 'Task created!',
       name: task.name,
