b983e3614f82b8ab0b7bd318d16f84db74cedae7
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-10-27 13:41:05 -0600

    forget RESTful

--- routes/api/users/index.ts
+++
@@ -1,9 +0,0 @@
-import { HandlerContext } from "$fresh/server.ts";
-import db from '../../../database/connectBD.ts';
-import { UserSchema } from '../../../schema/user.ts';
-import { create } from 'djwt';
-import * as bcrypt from 'bcrypt';
-import { key } from '../../../utils/apiKey.ts';
-import { ObjectId } from 'mongo';
-
-
