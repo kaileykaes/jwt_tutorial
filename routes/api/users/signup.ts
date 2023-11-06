b5c7f6a60cc03712c5dd1e0dbbc437df2895d582
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-11-06 15:27:04 -0700

    add missing password/username error handling

--- routes/api/users/signup.ts
+++ routes/api/users/signup.ts
@@ -8,18 +8,30 @@ const users = db.collection<UserSchema>('users');
 export const handler: Handlers<User | null> = {
   async POST(req, _ctx) {
     const {username, password} = (await req.json());
-    const salt = await bcrypt.genSalt(8);
-    const hashedPassword = await bcrypt.hash(password, salt);
+    if (!username ){
+      return new Response(JSON.stringify({message: "Username missing"}), {
+        status: 400,
+        statusText: "Bad Request"
+      });
+    } else if (!password){
+      return new Response(JSON.stringify({message: "Password missing"}), {
+        status: 400,
+        statusText: 'Bad Request'
+      });
+    } else {
+      const salt = await bcrypt.genSalt(8);
+      const hashedPassword = await bcrypt.hash(password, salt);
 
-    const _id = await users.insertOne({
-      username,
-      password: hashedPassword,
-    });
-    const body = {
-      message: 'User created',
-      userId: _id,
-      user: username };
+      const _id = await users.insertOne({
+        username,
+        password: hashedPassword,
+      });
+      const body = {
+        message: 'User created',
+        userId: _id,
+        user: username };
 
-    return new Response(JSON.stringify(body));
+      return new Response(JSON.stringify(body));
+    };
   }
 }
