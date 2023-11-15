f2426a43dace689fc3648aa5956720dd83c482b1
Joe Hildebrand <joe-github@cursive.net>
2023-11-15 08:29:28 -0700

    Script to clear database and TC export

---
+++ clearDatabase.ts
@@ -0,0 +1,8 @@
+#!/usr/bin/env -S deno run --unstable
+// WARNING: deletes the entire database
+
+import { Keyed } from './database/keyed.ts';
+
+for await (const {key} of Keyed.kv.list({prefix: []})) {
+  await Keyed.kv.delete(key);
+}
