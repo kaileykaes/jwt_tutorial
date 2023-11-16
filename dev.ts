19c50058bebb4b6a363589a2eda9437347e9841d
Joe Hildebrand <joe-github@cursive.net>
2023-11-16 12:50:11 -0700

    Resolve dangling timer so CI works.  Turn on CI caching.

--- dev.ts
+++ dev.ts
@@ -1,9 +1,11 @@
 #!/usr/bin/env -S deno run -A --watch=static/,routes/ --unstable
 
 import dev from '$fresh/dev.ts';
-
 import config from './fresh.config.ts';
-
 import '$std/dotenv/load.ts';
+import { Invalid } from './controllers/invalid.ts';
 
 await dev(import.meta.url, './main.ts', config);
+
+// Cleanup the dangling timer.
+Invalid.instance.stop();
