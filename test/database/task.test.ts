39bf15835044b1656d3b6ffefb7fab6e7b1016f6
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-11-24 13:57:34 -0700

    test task database

---
+++ test/database/task.test.ts
@@ -0,0 +1,19 @@
+import { assertRejects } from '$std/assert/assert_rejects.ts';
+import { failCommit } from './dbUtils.ts';
+import type { StoredTaskSchema } from '../../schema/task.ts'
+import { Task } from '../../database/task.ts';
+import { userId } from '../config.ts';
+
+Deno.test('Task', async(t) => {
+  let task: StoredTaskSchema | undefined = undefined;
+
+  await t.step('create', async () => {
+    //@ts-ignore: testing for bad inputs
+    await assertRejects(() => Task.create({}));
+
+    task = await Task.create({
+      userId: userId,
+      name: 'For real though, wash those greasy cats...today'
+    });
+  });
+})
