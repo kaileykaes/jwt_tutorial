061691e81483158e30748846d367597257ad28f4
Kailey Kaes <kaileykaes@gmail.com>
2024-01-16 15:29:58 -0700

    create zod schema for tasks

--- schema/task.ts
+++ schema/task.ts
@@ -1,3 +1,5 @@
+import { z } from 'zod';
+
 export interface TaskSchema {
   id?: string;
   userId: string;
@@ -6,3 +8,11 @@ export interface TaskSchema {
 }
 
 export type StoredTaskSchema = Required<TaskSchema>;
+
+// Task API
+// This is creating a zod object schema whereby zod can validate types
+
+export const TaskAPI = z.object({
+  userId: z.string().uuid(),
+  name: z.string().min(5).max(500),
+})
